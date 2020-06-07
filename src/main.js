b901f455f09eea440beaff77cb2f2b107952b080
钱程 <abcqdsdchina@126.com>
2020-06-07 17:27:00 +0800

    vue-app项目初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Button from 'vantx'
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+Vue.use(Button)
+
+const routes = [
+  { path: '/', component: Login },
+  { path: '/hello-world', component: HelloWorld },
+  { path: '/login', component: Login }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
