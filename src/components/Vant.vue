2af1d6d9e3ee37e7555a84db9450c8b97e826871
钱程 <abcqdsdchina@126.com>
2020-06-07 22:43:36 +0800

    vant演示组件

---
+++ src/components/Vant.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+      <van-cell v-for="item in list" :key="item">
+        <van-card num="2" price="2.00" desc="描述信息" title="商品标题" thumb="https://img.yzcdn.cn/vant/ipad.jpeg"/>
+      </van-cell>
+    </van-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vant',
+  data() {
+    return {
+      activeKey: 0,
+      list: [],
+      error: false,
+      loading: false,
+      finished: false
+    }
+  },
+  methods: {
+    onLoad() {
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1);
+        }
+
+        this.loading = false;
+
+        if (this.list.length >= 40) {
+          this.finished = true;
+        }
+      }, 1000);
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Button from 'vantx'
+import { Button } from 'vantx'
+import { Card } from 'vantx'
+import { Cell } from 'vantx'
+import { Col, Row } from 'vantx'
+import { List } from 'vantx'
+import { Sidebar, SidebarItem } from 'vantx'
 import App from './App.vue'
 import HelloWorld from './components/HelloWorld.vue'
 import Login from './components/Login.vue'
+import Vant from './components/Vant.vue'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(Button)
+Vue.use(Card)
+Vue.use(Cell)
+Vue.use(Col)
+Vue.use(Row)
+Vue.use(List)
+Vue.use(Sidebar)
+Vue.use(SidebarItem)
 
 const routes = [
   { path: '/', component: Login },
   { path: '/hello-world', component: HelloWorld },
-  { path: '/login', component: Login }
+  { path: '/login', component: Login },
+  { path: '/vant', component: Vant }
 ]
 
 const router = new VueRouter({
